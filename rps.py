49e01401563ad1c6283d57c2bc2c2c7ea1b5a7f1
sbisset <47271923+sbisset@users.noreply.github.com>
2019-05-26 20:23:51 +0100

    Add files via upload

---
+++ rps.py
@@ -0,0 +1,73 @@
+# RPS but python 
+
+import random 
+
+
+
+def variables():
+    global playerwins 
+    global compwins
+    playerwins = 0
+    compwins = 0 
+
+variables()
+
+
+def comp_choice(): 
+    global comp 
+    comp = random.randint(1,3)
+    
+
+    if comp == 1:
+        print ("Computer played: Rock")
+    elif comp == 2: 
+        print ("Computer played: Paper")
+    else :
+        print("Computer played: Scissors")
+
+
+	
+def player_choice()  :
+    print ("""1)Rock
+2)Paper
+3)Scissors""") 
+    global player 
+    player = int(input("Your choice?: "))
+
+
+
+def game(): 
+    if player +1 == comp:
+        print("You Lose!")
+        global compwins
+        compwins += 1
+    elif player == 3 and comp == 1: 
+        print ("You Lose!")
+        
+        compwins += 1
+    elif player == comp : 
+        print("Its a tie!")
+    else:
+        print("You Win !")
+        global playerwins
+        playerwins += 1 
+
+def printgamestats():
+    global compwins
+    print("Comp wins: ",compwins)
+    global playerwins
+    print("Player wins: ",playerwins)
+
+
+while True :
+    player_choice()
+    comp_choice()
+    game()
+    printgamestats()
+    if compwins == 3:
+        print ("Computer wins!")
+        break
+    elif playerwins == 3 :
+        print ("Player wins!")
+        break
+

6b17a90066e428ab419b6afe7b2bfc4b1be33921
sbisset <47271923+sbisset@users.noreply.github.com>
2019-05-29 16:21:50 +0100

    Added a premier inn search...
    Added it because why not?

--- mapit.py
+++ mapit.py
@@ -3,8 +3,9 @@ if len(sys.argv) > 1:
     # Get address from command line.
     address = ' '.join(sys.argv[1:])
     webbrowser.open('https://www.google.com/maps/place/' + address)
+    webbrowser.open('https://www.premierinn.com/gb/en/search.html?searchModel.searchTerm=' + address )
 else:
     # Get address from clipboard.
-    print("Usage:", sys.argv[0],"ADRESS")
-    exit
+    print("Usage:", sys.argv[0],"ADDRESS")
+    exit 
 

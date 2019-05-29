27a5be594e98eed767b0e6e1a4be2ea08ced189a
sbisset <47271923+sbisset@users.noreply.github.com>
2019-05-29 10:44:17 +0100

    Add files via upload

---
+++ mapit.py
@@ -0,0 +1,10 @@
+import webbrowser, sys 
+if len(sys.argv) > 1:
+    # Get address from command line.
+    address = ' '.join(sys.argv[1:])
+    webbrowser.open('https://www.google.com/maps/place/' + address)
+else:
+    # Get address from clipboard.
+    print("Usage:", sys.argv[0],"ADRESS")
+    exit 
+
